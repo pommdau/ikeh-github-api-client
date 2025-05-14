158b1e072dd6db4d075c3e43751e8cd955b496f0
IKEH1024 <pondau1024@gmail.com>
2025-05-14 13:53:53 +0900

    wip

--- Sources/IKEHGitHubAPIClient/Utils/URLSession/URLSession+URLSessionProtocol.swift
+++ Sources/IKEHGitHubAPIClient/Utils/URLSession/URLSession+URLSessionProtocol.swift
@@ -1,5 +1,8 @@
 import Foundation
 import HTTPTypesFoundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 /// - NOTE: デフォルトで実装済みのため準拠のみでOK
 extension URLSession: URLSessionProtocol {}
