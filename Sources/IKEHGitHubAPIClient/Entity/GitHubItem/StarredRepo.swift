bd790690dfe93f7de51c2c4df8ea87a1fad9a732
IKEH1024 <pondau1024@gmail.com>
2025-05-21 09:36:28 +0900

    chore: コード調整

--- Sources/IKEHGitHubAPIClient/Entity/GitHubItem/StarredRepo.swift
+++ Sources/IKEHGitHubAPIClient/Entity/GitHubItem/StarredRepo.swift
@@ -9,10 +9,16 @@ import Foundation
 
 /// スター済みリポジトリ
 public struct StarredRepo: GitHubItem {
+    
+    // MARK: - Definition
+    
     private enum CodingKeys: String, CodingKey {
         case starredAt = "starred_at"
         case repo
     }
+    
+    // MARK: - Property
+    
     /// スター日時
     public var starredAt: String
     /// リポジトリ
@@ -20,5 +26,14 @@ public struct StarredRepo: GitHubItem {
     
     // MARK: - Identifiable
     
+    /// id
     public var id: Int { repo.id }
+    
+    // MARK: - LifeCycle
+    
+    /// イニシャライザ
+    public init(starredAt: String, repo: Repo) {
+        self.starredAt = starredAt
+        self.repo = repo
+    }
 }
